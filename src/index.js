a752efffeaf52f0888e4be800af9298cf53c04be
darkspeaker <graggg13@gmail.com>
2021-03-15 22:25:19 +0200

    feat: add brackets 1 part

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let checkString = str;
+    let stack = []
+    for(let i = 0; i <= checkString.length; i++){
+        if(checkString[i] === '(' || checkString[i] === '[' || checkString[i] === '{'){
+            stack.push(checkString[i]);
+            console.log(stack)
+        }
+        else if(checkString[i] === ')' || checkString[i] === ']' || checkString[i] === '}'){
+            if(stack.length > 0){
+                stack.pop()
+            }
+            else{
+                return false
+            }
+        }
+    }
+   
+    if(stack.pop()) return false;
+    return true;
 }
